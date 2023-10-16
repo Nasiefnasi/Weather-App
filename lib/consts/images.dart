9fa2af005c8e823390341d849079bd6935329503
muhammad nasief <nasinasief@gmail.com>
2023-10-16 15:27:20 +0530

    home UI completed

--- lib/consts/images.dart
+++ lib/consts/images.dart
@@ -1,1 +1,3 @@
-// const String clouds= ""
+const String clouds= "assets/icons/clouds.png";
+const String humidly= "assets/icons/humidity.png";
+const String windspeed= "assets/icons/windspeed.png";
--- lib/design/weatherapp.dart
+++ lib/design/weatherapp.dart
@@ -1,10 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:weatherapp_starter_project/consts/colors.dart';
+import 'package:weatherapp_starter_project/consts/images.dart';
+import 'package:weatherapp_starter_project/consts/string.dart';
 
 class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Column());
+    var date = DateFormat("yMMMMd").format(DateTime.now());
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: "$date".text.gray700.make(),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.light_mode,
+              color: Vx.gray600,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+              color: Vx.gray600,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(12),
+        child: SingleChildScrollView(physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "LAHORE"
+                  .text
+                  .fontFamily('poppins_black')
+                  .size(36)
+                  .letterSpacing(3)
+                  .make(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    "assets/weather/01d.png",
+                    width: 80,
+                    height: 80,
+                  ),
+                  RichText(
+                      text: const TextSpan(children: [
+                    TextSpan(
+                      text: "37$degree",
+                      style: TextStyle(
+                          fontFamily: "poppins", color: Vx.gray900, fontSize: 64),
+                    ),
+                    TextSpan(
+                      text: "Sunny",
+                      style: TextStyle(
+                          fontFamily: "poppins_light",
+                          color: Vx.gray900,
+                          fontSize: 14,
+                          letterSpacing: 3),
+                    ),
+                  ]))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton.icon(
+                      onPressed: null,
+                      icon: const Icon(
+                        Icons.expand_less_rounded,
+                        color: Vx.gray400,
+                      ),
+                      label: "41$degree".text.make()),
+                  TextButton.icon(
+                      onPressed: null,
+                      icon: const Icon(
+                        Icons.expand_less_rounded,
+                        color: Vx.gray400,
+                      ),
+                      label: "26$degree".text.make()),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(3, (index) {
+                  var iconsList = [clouds, humidly, windspeed];
+                  var values = ["70%", "40%", "3.5km/h"];
+                  return Column(
+                    children: [
+                      Image.asset(
+                        iconsList[index],
+                        width: 60,
+                        height: 60,
+                      )
+                          .box
+                          .gray200
+                          .padding(const EdgeInsets.all(8))
+                          .roundedSM
+                          .make(),
+                      10.heightBox,
+                      values[index].text.gray400.make()
+                    ],
+                  );
+                }),
+              ),
+              10.heightBox,
+              const Divider(),
+              10.heightBox,
+              SizedBox(
+                height: 150,
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.only(right: 4),
+                      decoration: BoxDecoration(
+                        color: cardcolor,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(children: [
+                        "${index + 1} AM".text.gray200.make(),
+                        Image.asset(
+                          "assets/weather/10n.png",
+                          width: 80,
+                        ),
+                        "38$degree".text.white.make()
+                      ]),
+                    );
+                  },
+                ),
+              ),
+              10.heightBox,
+              const Divider(),
+              10.heightBox,
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  "Next 7 Days".text.semiBold.size(16).make(),
+                  TextButton(onPressed: () {}, child: "view all".text.make())
+                ],
+              ),
+              ListView.builder(itemCount: 7, itemBuilder: (context, index) {var day = DateFormat("EEEE").format(DateTime.now().add(Duration(days: index + 1)));
+                return Card(child: Container(padding: EdgeInsets.symmetric(horizontal: 8,vertical: 12),child: Row(children: ["$day".text.make()]),),);
+              },)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
